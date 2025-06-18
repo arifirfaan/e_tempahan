c45e6ac47cea5e419bbcebf4a1324abcf010578a
Arif Irfaan <arifirfaanworks@gmail.com>
2025-06-18 11:41:55 +0800

    update input style

--- lib/views/view_form.dart
+++ lib/views/view_form.dart
@@ -13,20 +13,47 @@ class ViewForm extends StatefulWidget {
 
 class _ViewFormState extends State<ViewForm> {
   final TextEditingController _controllerName = TextEditingController();
+  final TextEditingController _controllerPhone = TextEditingController();
 
   String labelName = "";
+  int labelPhone = 0;
 
   /// Fungsi untuk gantikan nama dari nilai TextField
   void onSubmit() {
     setState(() {
       labelName = _controllerName.text;
+
+      // check if user input alphabet in phone input
+      if (RegExp(r'[a-zA-Z]').hasMatch(_controllerPhone.text)) {
+        showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: Text("Wrong Input"),
+                content: Text("Please input the correct number only."),
+                actions: [
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text("Continue")
+                  )
+                ],
+              );
+            });
+      } else {
+        labelPhone = int.parse(_controllerPhone.text);
+      }
+
       _controllerName.clear();
+      _controllerPhone.clear();
     });
   }
 
   @override
   void dispose() {
     _controllerName.dispose();
+    _controllerPhone.dispose();
     super.dispose();
   }
 
@@ -39,6 +66,7 @@ class _ViewFormState extends State<ViewForm> {
           children: [
             Icon(Icons.person_pin),
             Text("Hello " + labelName),
+            Text("Your phone number is $labelPhone"),
             Container(
               height: 400,
               width: 300,
@@ -48,10 +76,12 @@ class _ViewFormState extends State<ViewForm> {
               child: ListView(
                 children: [
                   TextField(
-                    decoration: InputDecoration(
-                      hintText: "Enter Name"
-                    ),
+                    decoration: InputDecoration(hintText: "Enter Name"),
                     controller: _controllerName,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(hintText: "Enter Phone"),
+                    controller: _controllerPhone,
                   )
                 ],
               ),
